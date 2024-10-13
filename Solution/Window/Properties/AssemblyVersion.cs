d248cff9a08d28e930dacb3cd0915a4d565632ec
Lawrence <30965946+Halbann@users.noreply.github.com>
2024-10-13 11:28:38 +0100

    Fix version file.

--- Solution/Window/Properties/AssemblyVersion.cs
+++ Solution/Window/Properties/AssemblyVersion.cs
@@ -5,6 +5,6 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.0.0.0")]
- [assembly: AssemblyFileVersion("1.0.0.0")]
- [assembly: KSPAssembly("Window", 1, 0, 0)]
+ [assembly: AssemblyVersion("1.0.1.0")]
+ [assembly: AssemblyFileVersion("1.0.1.0")]
+ [assembly: KSPAssembly("Window", 1, 0, 1)]
